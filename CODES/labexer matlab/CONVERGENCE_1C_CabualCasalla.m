8becc8bb9fa4ce2f13e07e57469db13fa4d3ff5a
ChinoKaffu <74938052+ChinoKaffu@users.noreply.github.com>
2023-03-05 19:36:41 +0800

    Finalized 1C, ready for submission

--- CODES/labexer matlab/CONVERGENCE_1C_CabualCasalla.m
+++ CODES/labexer matlab/CONVERGENCE_1C_CabualCasalla.m
@@ -6,13 +6,11 @@
 %FUNCTIONS
 
     function convergence()
-        % Display the initial question
         question = 'Is the convergence for Jacobi guaranteed? Why or why not?';
         button = questdlg(question, 'Question', 'Answer', 'Cancel', 'Answer');
 
-        % If the 'Answer' button is clicked, display the answer
         if strcmp(button, 'Answer')
-            answer = 'The convergence of the Jacobi method is guaranteed under certain conditions. Specifically, the method is guaranteed to converge if the matrix A is strictly diagonally dominant, which means that the absolute value of the diagonal element in each row is greater than the sum of the absolute values of the other elements in that row. Under this condition, the Jacobi method will converge to the solution of the linear system, regardless of the initial guess. This is because the method involves iteratively updating the solution vector by using the diagonal elements of A, which are guaranteed to be non-zero and therefore invertible. However, if the matrix A is not strictly diagonally dominant, the Jacobi method may not converge. In such cases, other iterative methods such as the Gauss-Seidel method or the successive over-relaxation (SOR) method may be more effective.';
+            answer = 'The convergence of the Jacobi method is not always guaranteed. Specifically, the method is guaranteed to converge if the matrix is strictly, and is only, diagonally dominant. This means that the absolute value of the diagonal element in each row is greater than the sum of the absolute values of the other elements in that row. Under this condition, the Jacobi method will converge to the solution of the linear system, regardless of the initial guess. With our observation, if the matrix is not diagonally dominant, the solution will diverge or oscillate rather than converge to the solution.';
             msgbox(answer, 'Answer');
         end
     end
